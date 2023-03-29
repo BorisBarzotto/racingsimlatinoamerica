9d097ba5f97484c29d00db867de946c57fa93da6
BorisBarzotto <fakieboris@gmail.com>
2023-03-28 22:13:47 -0400

    app useEffect added for load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/styles/globals.css'
+import { useState, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
   return (
+    <div style={{ visibility: !mounted ? 'hidden' : 'visible' }}>
       <Component {...pageProps} />
+      </div>
   )
 }
