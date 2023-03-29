569721439e71cf60ce115d54f605f249cf3ddf79
BorisBarzotto <fakieboris@gmail.com>
2023-03-28 21:39:10 -0400

    created custom document, bg image component convert

--- components/HistoricalTable.tsx
+++ components/HistoricalTable.tsx
@@ -21,7 +21,7 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       dataIndex: 'piloto',
       key: 'piloto',
       align: "left",
-      width: "35%",
+
       render: ((text) =>
         <Typography className={myFont.className} style={!screens.sm ? { fontSize:'0.7rem', fontWeight: 300 }:{ fontWeight: 300 }}>{text}</Typography>
       )
@@ -31,7 +31,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       dataIndex: 'pais',
       key: 'pais',
       align: "center",
-      width: "6%",
       render: ((text) =>
         <Image src={`/flag/${text}.jpg`} width={34} height={20} alt={text} />
       )
@@ -42,7 +41,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'primero',
       sorter: !screens.sm ? false : (a: { primero: number; }, b: { primero: number; }) => a.primero - b.primero,
       align: "center",
-      width: !screens.sm ? "5%" : '11%',
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
       )
@@ -53,7 +51,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'segundo',
       sorter: !screens.sm ? false : (a: { segundo: number; }, b: { segundo: number; }) => a.segundo - b.segundo,
       align: "center",
-      width: !screens.sm ? "5%" : '10%',
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
       )
@@ -64,7 +61,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'tercero',
       sorter: !screens.sm ? false : (a: { tercero: number; }, b: { tercero: number; }) => a.tercero - b.tercero,
       align: "center",
-      width: !screens.sm ? "5%" : '10%',
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
       )
@@ -75,7 +71,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'podios',
       sorter: (a: { podios: number; }, b: { podios: number; }) => a.podios - b.podios,
       align: "center",
-      width: !screens.sm ? "5%" : '10%',
       responsive: ['sm'],
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
@@ -87,7 +82,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'pp',
       sorter: !screens.sm ? false : (a: { pp: number; }, b: { pp: number; }) => a.pp - b.pp,
       align: "center",
-      width: !screens.sm ? "5%" : '10%',
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
       )
@@ -98,7 +92,6 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
       key: 'vr',
       sorter: !screens.sm ? false : (a: { vr: number; }, b: { vr: number; }) => a.vr - b.vr,
       align: "center",
-      width: !screens.sm ? "5%" : '11%',
       render: ((text) =>
         <Typography className={myFont.className} style={{ fontWeight: 400 }}>{text}</Typography>
       )
@@ -107,12 +100,16 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
 
 
   return (
-    <div style={!screens.sm ? { margin: '3em auto', boxShadow: "rgba(17, 17, 26, 0.1) 0px 0px 16px" } : { maxWidth: 720, margin: '3em auto', boxShadow: "rgba(17, 17, 26, 0.1) 0px 0px 16px" }}>
+    <>
+    <div style={{zIndex:-1, position:'fixed', width:'100vw', height:'100vh', top:0,left:0,right:0,bottom:0}}>
+      <Image src={"/fondo.jpg"} alt={"Circuit background"} fill style={{objectFit:"cover"}} />
+    </div>
+    <div style={!screens.sm ? { padding: '0.2em', marginInline:"auto", boxShadow: "rgba(17, 17, 26, 0.1) 0px 0px 16px" } : { maxWidth: 720, padding: '3em',marginInline:"auto", boxShadow: "rgba(17, 17, 26, 0.1) 0px 0px 16px" }}>
       <Table
         title={() => (
           <>
             <div style={{ display: 'flex', flexDirection: 'row', padding: "0.5em 0em 0.5em 0.5em" }}>
-              <Image style={{ filter: "drop-shadow(2px 2px 5px #262626)" }} src="/srl-logo.png" width={80} height={80} alt={"logo simracinglatinoamerica"} />
+              <Image src="/srl-logo.png" width={80} height={80} alt={"logo simracinglatinoamerica"} />
               <h1>DTM</h1>
             </div>
             <Divider style={{ backgroundColor: "#ddfe81", margin: 0 }} />
@@ -126,6 +123,7 @@ export const HistoricalTable: FunctionComponent<HistoricalTableProps> = ({ dtm }
         rowKey={(dtm) => dtm.key}
       />
     </div>
+    </>
   )
 }
 
